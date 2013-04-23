8dff3cb997ef859ae1875a70077d7a2d5044119a
magau <magau.macedo@gmail.com>
2013-04-24 00:52:57 +0100

    not working yet...

--- obj/i686Release/addons/ofxOsc/libs/oscpack/src/ip/win32/NetworkingUtilsWin.d
+++
@@ -1,5 +0,0 @@
-obj/i686Release/addons/ofxOsc/libs/oscpack/src/ip/win32/NetworkingUtilsWin.o: \
- ../../../addons/ofxOsc/libs/oscpack/src/ip/win32/NetworkingUtilsWin.cpp \
- ../../../addons/ofxOsc/libs/oscpack/src/ip/NetworkingUtils.h
-
-../../../addons/ofxOsc/libs/oscpack/src/ip/NetworkingUtils.h:
--- obj/i686Release/addons/ofxOsc/libs/oscpack/src/osc/OscTypes.d
+++
@@ -1,5 +0,0 @@
-obj/i686Release/addons/ofxOsc/libs/oscpack/src/osc/OscTypes.o: \
- ../../../addons/ofxOsc/libs/oscpack/src/osc/OscTypes.cpp \
- ../../../addons/ofxOsc/libs/oscpack/src/osc/OscTypes.h
-
-../../../addons/ofxOsc/libs/oscpack/src/osc/OscTypes.h:
